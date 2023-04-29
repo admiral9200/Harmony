39e62b828c485be2ddba469d7266c0fc2d59de34
Whil <miltonivan117@gmail.com>
2023-04-28 23:52:51 -0600

    randomcolors

--- src/utils/randomColor/index.tsx
+++ src/utils/randomColor/index.tsx
@@ -4,3 +4,11 @@ export const getRandomColor = () => {
   const b = Math.floor(Math.random() * 156) + 100;
   return "#" + r.toString(16) + g.toString(16) + b.toString(16);
 };
+
+export const getRandomsColors = (limit: number) => {
+  let colors = [];
+  for (let index = 0; index < limit; index++) {
+    colors.push(getRandomColor());
+  }
+  return colors;
+};
