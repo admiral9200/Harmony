ab2e18d02387a1bf9ec8a67166422fe171affe04
Whil <miltonivan117@gmail.com>
2023-04-28 21:50:17 -0600

    move and component editorscreen

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -1,54 +1,39 @@
-import { Circle, Layer, Rect, Stage, Text } from "react-konva";
+import { Circle, Layer, Rect, Text } from "react-konva";
 
-import useScreen from "@/hooks/useScreen";
-import { AtomWrapper } from "lucy-nxtjs";
+import AtomEditorScreen from "@/editor/Stage";
 import { NextOnlyPage } from "next";
 
-const PageEditor: NextOnlyPage = (props) => {
-  const screen = useScreen({
-    deps: [],
-  });
-
+const PageEditor: NextOnlyPage = () => {
   return (
-    <AtomWrapper
-      ref={screen.ref}
-      customCSS={(css) => css`
-        overflow: none;
-      `}
-    >
-      <Stage
-        width={screen?.dimensions.width}
-        height={screen?.dimensions.height}
-      >
-        <Layer>
-          <Rect
-            width={50}
-            height={50}
-            fill="red"
-            draggable
-            onClick={() => {
-              console.log("SDF");
-            }}
-          />
-          <Circle
-            x={200}
-            y={200}
-            stroke="black"
-            radius={50}
-            draggable
-            fill="red"
-          />
-          <Text
-            fontSize={24}
-            // fontStyle="bold"
-            text="Heyt "
-            wrap="char"
-            draggable
-            align="center"
-          />
-        </Layer>
-      </Stage>
-    </AtomWrapper>
+    <AtomEditorScreen>
+      <Layer>
+        <Rect
+          width={50}
+          height={50}
+          fill="red"
+          draggable
+          onClick={() => {
+            console.log("SDF");
+          }}
+        />
+        <Circle
+          x={200}
+          y={200}
+          stroke="black"
+          radius={50}
+          draggable
+          fill="red"
+        />
+        <Text
+          fontSize={24}
+          // fontStyle="bold"
+          text="Heyt "
+          wrap="char"
+          draggable
+          align="center"
+        />
+      </Layer>
+    </AtomEditorScreen>
   );
 };
 
