b51c164db583a27a85f688a40f53542415fd3126
Whil <miltonivan117@gmail.com>
2023-04-29 21:31:15 -0600

    feat: add list elements sidebar left

--- src/components/layout/editor/sidebar/left/index.tsx
+++ src/components/layout/editor/sidebar/left/index.tsx
@@ -1,14 +1,16 @@
-import { AtomText, AtomWrapper } from "lucy-nxtjs";
+import { AtomWrapper } from "lucy-nxtjs";
 import { FC } from "react";
+import ElementsList from "./elements";
 
 const LayoutEditorSidebarLeft: FC = () => {
   return (
     <AtomWrapper
       backgroundColor="#202020"
       justifyContent="flex-start"
+      alignItems="flex-start"
       padding="1.3rem"
     >
-      <AtomText color="white">LayoutEditorSidebarLeft</AtomText>
+      <ElementsList />
     </AtomWrapper>
   );
 };
