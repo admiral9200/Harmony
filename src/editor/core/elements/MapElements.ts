ad35a3f9693edd037f2f3210f96d3400c4976ef7
Whil <miltonivan117@gmail.com>
2023-05-01 16:52:47 -0600

    add map elements CIRCLE AND LINE

--- src/editor/core/elements/MapElements.ts
+++ src/editor/core/elements/MapElements.ts
@@ -1,5 +1,7 @@
 import { IKeyTool } from "../tools/types";
 import AtomEditorElementBox from "./BOX";
+import AtomElementCircle from "./CIRCLE";
+import AtomElementLine from "./LINE";
 import AtomElementText from "./TEXT";
 import { IFCElement } from "./type";
 
@@ -11,6 +13,8 @@ const mapperElements = (): IMapperElements => {
   return {
     BOX: AtomEditorElementBox,
     TEXT: AtomElementText,
+    CIRCLE: AtomElementCircle,
+    LINE: AtomElementLine,
   };
 };
 
