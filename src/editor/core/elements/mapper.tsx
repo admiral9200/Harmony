abf5734db93495cac41526e7db016f9dc53ace4e
Whil <miltonivan117@gmail.com>
2023-04-30 22:37:01 -0600

    refactor and add hooks editor mapper main component layer

--- src/editor/core/elements/mapper.tsx
+++ src/editor/core/elements/mapper.tsx
@@ -1,15 +1,34 @@
-import useEditorStatement from "@/hooks/useStatement";
+import useElement from "@/hooks/useElement";
+import useElements from "@/hooks/useStatement";
 import { Layer } from "react-konva";
 import { mapperElements } from "./MapElements";
 import { FCE } from "./type";
 
 const AtomEditorMapper = () => {
-  const { elements, draggable } = useEditorStatement();
+  const { elements, draggable } = useElements();
+
+  const { element, setElement, upElement } = useElement();
+  console.log({ elements });
+
   return (
     <Layer>
       {elements?.map((item) => {
         const Component = mapperElements?.()?.[item.tool] as FCE;
-        return <Component key={item.id} {...item} draggable={draggable} />;
+        return (
+          <Component
+            {...item}
+            key={item.id}
+            draggable={draggable}
+            isSelected={item?.id === element?.id}
+            onChange={(data) => {
+              upElement(data);
+            }}
+            onSelect={(data) => {
+              setElement(data);
+              upElement(data);
+            }}
+          />
+        );
       })}
     </Layer>
   );
