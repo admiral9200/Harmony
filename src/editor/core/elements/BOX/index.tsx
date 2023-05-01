fb82546ecaed1c909573800ad9f70cd89b2e7d0f
Whil <miltonivan117@gmail.com>
2023-04-30 22:38:08 -0600

    Add Element box transform with draggin element

--- src/editor/core/elements/BOX/index.tsx
+++ src/editor/core/elements/BOX/index.tsx
@@ -1,19 +1,75 @@
-import { Rect } from "react-konva";
+import Konva from "konva";
+import { MutableRefObject, useEffect, useRef } from "react";
+import { Rect, Transformer } from "react-konva";
 import { IFCElement } from "../type";
 
-const AtomEditorElementBox = ({ x, y, draggable }: IFCElement) => {
+const AtomEditorElementBox = (item: IFCElement) => {
+  const { draggable, onChange, onSelect, isSelected } = item;
+  const shapeRef = useRef<Konva.Rect>();
+  const trRef = useRef<Konva.Transformer>();
+
+  useEffect(() => {
+    if (isSelected) {
+      // we need to attach transformer manually
+      if (trRef.current && shapeRef.current) {
+        trRef.current.nodes([shapeRef.current]);
+        trRef.current?.getLayer()?.batchDraw();
+      }
+    }
+  }, [isSelected]);
   return (
-    <Rect
-      x={x}
-      y={y}
-      width={50}
-      height={50}
-      fill="red"
-      draggable={draggable}
-      onClick={() => {
-        console.log("Box");
-      }}
-    />
+    <>
+      <Rect
+        {...item}
+        ref={shapeRef as MutableRefObject<Konva.Rect>}
+        draggable={draggable}
+        onClick={() => onSelect(item)}
+        onTap={() => onSelect(item)}
+        onDragEnd={(e) => {
+          onChange({
+            ...item,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        onTransformEnd={(e) => {
+          // transformer is changing scale of the node
+          // and NOT its width or height
+          // but in the store we have only width and height
+          // to match the data better we will reset scale on transform end
+
+          if (shapeRef?.current) {
+            const node = shapeRef.current;
+            const scaleX = node.scaleX();
+            const scaleY = node.scaleY();
+
+            // we will reset it back
+            node.scaleX(1);
+            node.scaleY(1);
+            onChange({
+              ...item,
+              x: node.x(),
+              y: node.y(),
+              // set minimal value
+              width: Math.max(5, node.width() * scaleX),
+              height: Math.max(node.height() * scaleY),
+            });
+          }
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef as MutableRefObject<Konva.Transformer>}
+          boundBoxFunc={(oldBox, newBox) => {
+            // limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </>
   );
 };
 
