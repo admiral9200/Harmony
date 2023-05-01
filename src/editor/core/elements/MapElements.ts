b25242b507d01ef199e56927974319bef25ff4a3
Whil <miltonivan117@gmail.com>
2023-05-01 12:35:09 -0600

    Add New Map Element

--- src/editor/core/elements/MapElements.ts
+++ src/editor/core/elements/MapElements.ts
@@ -1,5 +1,6 @@
 import { IKeyTool } from "../tools/types";
 import AtomEditorElementBox from "./BOX";
+import AtomElementText from "./TEXT";
 import { IFCElement } from "./type";
 
 export type IMapperElements = {
@@ -9,6 +10,7 @@ export type IMapperElements = {
 const mapperElements = (): IMapperElements => {
   return {
     BOX: AtomEditorElementBox,
+    TEXT: AtomElementText,
   };
 };
 
