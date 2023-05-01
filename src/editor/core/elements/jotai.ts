2a41adf598626405873ef32d58a0f50d1d76d937
Whil <miltonivan117@gmail.com>
2023-04-30 22:31:10 -0600

    feat mapper state elements

--- src/editor/core/elements/jotai.ts
+++ src/editor/core/elements/jotai.ts
@@ -3,4 +3,24 @@ import { IElement } from "./type";
 
 const ElementsAtom = atom<IElement[]>([]);
 
-export { ElementsAtom };
+const ElementSelected = atom<IElement>({} as IElement);
+
+const remapping = (element: IElement, data: IElement[]): IElement[] => {
+  const map = new Map<string, IElement>();
+  for (const iterator of data) {
+    if (element.id === iterator.id) {
+      map.set(element.id, element);
+    } else {
+      map.set(iterator.id, iterator);
+    }
+  }
+  return Array.from(map.values());
+};
+
+const setElsUpAtom = atom(null, (get, set, args: IElement) => {
+  const reMap = remapping(args, get(ElementsAtom));
+  set(ElementsAtom, reMap);
+  set(ElementSelected, args);
+});
+
+export { ElementsAtom, ElementSelected, setElsUpAtom };
