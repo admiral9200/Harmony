975f7b1e79c5e36a4947e7fbb4ac68b189aeca3b
Whil <miltonivan117@gmail.com>
2023-05-01 21:32:04 -0600

    update image  with transform

--- src/editor/core/elements/IMAGE/index.tsx
+++ src/editor/core/elements/IMAGE/index.tsx
@@ -1,12 +1,78 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { Image as KonvaImg } from "react-konva";
+import Konva from "konva";
+import { MutableRefObject, useEffect, useRef } from "react";
+import { Image as KonvaImg, Transformer } from "react-konva";
 import { IFCElement } from "../type";
 
 const AtomElementImage = (item: IFCElement) => {
   const { x, y } = item;
   const image = new Image();
   image.src = item.src as string;
-  return <KonvaImg x={x} y={y} image={image} />;
+
+  const { rotate, draggable, onChange, onSelect, isSelected } = item;
+  const shapeRef = useRef<Konva.Image>();
+  const trRef = useRef<Konva.Transformer>();
+
+  useEffect(() => {
+    if (isSelected) {
+      if (trRef.current && shapeRef.current) {
+        trRef.current.nodes([shapeRef.current]);
+        trRef.current?.getLayer()?.batchDraw();
+      }
+    }
+  }, [isSelected]);
+
+  return (
+    <>
+      <KonvaImg
+        x={x}
+        y={y}
+        image={image}
+        ref={shapeRef as MutableRefObject<Konva.Image>}
+        draggable={draggable}
+        onClick={() => onSelect(item)}
+        onTap={() => onSelect(item)}
+        rotation={rotate}
+        onDragEnd={(e) => {
+          onChange({
+            ...item,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        onTransformEnd={(e) => {
+          const rotate = e.target.rotation();
+          if (shapeRef?.current) {
+            const node = shapeRef.current;
+            const scaleX = node.scaleX();
+            const scaleY = node.scaleY();
+
+            onChange({
+              ...item,
+              x: node.x(),
+              y: node.y(),
+              rotate,
+              width: Math.max(5, node.width() * scaleX),
+              height: Math.max(node.height() * scaleY),
+            });
+          }
+        }}
+      />
+      {isSelected && (
+        <Transformer
+          ref={trRef as MutableRefObject<Konva.Transformer>}
+          keepRatio={false}
+          boundBoxFunc={(oldBox, newBox) => {
+            // limit resize
+            if (newBox.width < 5 || newBox.height < 5) {
+              return oldBox;
+            }
+            return newBox;
+          }}
+        />
+      )}
+    </>
+  );
 };
 
 export default AtomElementImage;
