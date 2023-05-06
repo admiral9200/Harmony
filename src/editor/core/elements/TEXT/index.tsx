67b22cc56eb07ce18ae07e9cc6661490eeabc08b
Whil <miltonivan117@gmail.com>
2023-05-05 21:00:31 -0600

    update text element

--- src/editor/core/elements/TEXT/index.tsx
+++ src/editor/core/elements/TEXT/index.tsx
@@ -1,6 +1,6 @@
 import Konva from "konva";
 import { MutableRefObject, useEffect, useRef } from "react";
-import { Group, Rect, Text, Transformer } from "react-konva";
+import { Text, Transformer } from "react-konva";
 import { IFCElement } from "../type";
 
 const AtomElementText = (item: IFCElement) => {
@@ -15,12 +15,11 @@ const AtomElementText = (item: IFCElement) => {
     height,
     style,
   } = item;
-  const shapeRef = useRef<Konva.Rect>();
+  const shapeRef = useRef<Konva.Text>();
   const trRef = useRef<Konva.Transformer>();
 
   useEffect(() => {
     if (isSelected) {
-      // we need to attach transformer manually
       if (trRef.current && shapeRef.current) {
         trRef.current.nodes([shapeRef.current]);
         trRef.current?.getLayer()?.batchDraw();
@@ -29,68 +28,50 @@ const AtomElementText = (item: IFCElement) => {
   }, [isSelected]);
   return (
     <>
-      <Group
-        onClick={() => onSelect(item)}
+      <Text
+        x={x}
+        y={y}
+        width={width}
+        height={height}
+        fontSize={12}
+        ref={shapeRef as MutableRefObject<Konva.Text>}
         draggable={draggable}
+        onClick={() => onSelect(item)}
         onTap={() => onSelect(item)}
-      >
-        <Rect
-          x={x}
-          y={y}
-          fill={style?.backgroundColor}
-          width={width}
-          height={height}
-          ref={shapeRef as MutableRefObject<Konva.Rect>}
-          // draggable={draggable}
-          // onClick={() => onSelect(item)}
-          // onTap={() => onSelect(item)}
-          zIndex={2}
-          onDragEnd={(e) => {
+        zIndex={2}
+        onDragEnd={(e) => {
+          onChange({
+            ...item,
+            x: e.target.x(),
+            y: e.target.y(),
+          });
+        }}
+        onTransformEnd={(e) => {
+          if (shapeRef?.current) {
+            const rotate = e.target.rotation();
+            const node = shapeRef.current;
+            const scaleX = node.scaleX();
+            const scaleY = node.scaleY();
+            node.scaleX(1);
+            node.scaleY(1);
             onChange({
               ...item,
-              x: e.target.x(),
-              y: e.target.y(),
+              x: node.x(),
+              y: node.y(),
+              rotate,
+              width: Math.max(5, node.width() * scaleX),
+              height: Math.max(node.height() * scaleY),
             });
-          }}
-          onTransformEnd={(e) => {
-            if (shapeRef?.current) {
-              const rotate = e.target.rotation();
-              const node = shapeRef.current;
-              const scaleX = node.scaleX();
-              const scaleY = node.scaleY();
-              // we will reset it back
-              node.scaleX(1);
-              node.scaleY(1);
-              onChange({
-                ...item,
-                x: node.x(),
-                y: node.y(),
-                rotate,
-                width: Math.max(5, node.width() * scaleX),
-                height: Math.max(node.height() * scaleY),
-              });
-            }
-          }}
-        />
-        <Text
-          x={x}
-          y={y}
-          width={width}
-          height={height}
-          // align="center"
-          fontSize={12}
-          // fill={isDarkLight(`${fill}`)}
-          fill={style?.colorText}
-          text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus laborum sunt, officia, veritatis iure sapiente assumenda ipsum ducimus quasi atque est delectus neque harum ullam non ex! Nulla, temporibus eveniet?"
-        />
-      </Group>
-
+          }
+        }}
+        fill={style?.colorText}
+        text="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus laborum sunt, officia, veritatis iure sapiente assumenda ipsum ducimus quasi atque est delectus neque harum ullam non ex! Nulla, temporibus eveniet?"
+      />
       {isSelected && (
         <Transformer
           ref={trRef as MutableRefObject<Konva.Transformer>}
           keepRatio={false}
           boundBoxFunc={(oldBox, newBox) => {
-            // limit resize
             if (newBox.width < 5 || newBox.height < 5) {
               return oldBox;
             }
