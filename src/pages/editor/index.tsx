1958e27e6ef653003ed4eed2c711164d3db7bdb6
Whil <miltonivan117@gmail.com>
2023-05-07 00:53:59 -0600

    delete code commented

--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -6,33 +6,6 @@ const PageEditor: NextOnlyPage = () => {
   return (
     <AtomEditorScreen>
       <AtomEditorMapper />
-      {/* <Rect
-          x={242}
-          y={80}
-          width={50}
-          height={50}
-          fill="red"
-          draggable
-          onClick={() => {
-            console.log("SDF");
-          }}
-        />
-        <Circle
-          x={200}
-          y={200}
-          stroke="black"
-          radius={50}
-          draggable
-          fill="red"
-        />
-        <Text
-          fontSize={24}
-          // fontStyle="bold"
-          text="Heyt "
-          wrap="char"
-          draggable
-          align="center"
-        /> */}
     </AtomEditorScreen>
   );
 };
