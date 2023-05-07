d0917012b12c81507ca62b503c3873da9c1dd304
Whil <miltonivan117@gmail.com>
2023-05-07 12:58:12 -0600

    update allupdate method useelemt

--- src/hooks/useElement/index.ts
+++ src/hooks/useElement/index.ts
@@ -1,14 +1,20 @@
 import { ElementSelected, setElsUpAtom } from "@/editor/core/elements/jotai";
+import { IElement } from "@/editor/core/elements/type";
 import { useAtom, useSetAtom } from "jotai";
 
 const useElement = () => {
   const [element, setElement] = useAtom(ElementSelected);
 
   const setReUpdate = useSetAtom(setElsUpAtom);
+
   return {
     element,
     setElement,
     upElement: setReUpdate,
+    allUpdate: (element: IElement) => {
+      setReUpdate(element);
+      setElement(element);
+    },
   };
 };
 export default useElement;
