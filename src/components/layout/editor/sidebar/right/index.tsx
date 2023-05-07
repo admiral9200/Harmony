40c60a88396179d8123669341d7285bc16b07f6c
Whil <miltonivan117@gmail.com>
2023-05-07 00:56:07 -0600

    add component main sidebaright

--- src/components/layout/editor/sidebar/right/index.tsx
+++ src/components/layout/editor/sidebar/right/index.tsx
@@ -1,10 +1,16 @@
-import { AtomText, AtomWrapper } from "lucy-nxtjs";
+import { AtomWrapper } from "lucy-nxtjs";
 import { FC } from "react";
+import SidebarTabsRight from "./tabs";
 
 const LayoutEditorSidebarRight: FC = () => {
   return (
-    <AtomWrapper backgroundColor="#202020">
-      <AtomText color="white">LayoutEditorSidebarRight</AtomText>
+    <AtomWrapper
+      backgroundColor="#202020"
+      justifyContent="flex-start"
+      alignItems="flex-start"
+      padding="1.3rem"
+    >
+      <SidebarTabsRight />
     </AtomWrapper>
   );
 };
