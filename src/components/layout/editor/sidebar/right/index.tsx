6c675fc61e91f35f80fbdf88342dd96a00f8e5e4
Whil <miltonivan117@gmail.com>
2023-05-07 12:52:00 -0600

    change sidebarmmain component with detect element

--- src/components/layout/editor/sidebar/right/index.tsx
+++ src/components/layout/editor/sidebar/right/index.tsx
@@ -1,8 +1,20 @@
+import { IKeyTool } from "@/editor/core/tools/types";
+import useElement from "@/hooks/useElement";
 import { AtomWrapper } from "lucy-nxtjs";
 import { FC } from "react";
-import SidebarTabsRight from "./tabs";
+import LayoutSidebarRightStageTabElementBox from "./tabs/elements/BOX";
+import StageSidebarRight from "./tabs/stage";
+
+type LayoutsTabs = {
+  [key in IKeyTool]?: JSX.Element;
+};
+
+const layoutTabs: LayoutsTabs = {
+  BOX: <LayoutSidebarRightStageTabElementBox />,
+};
 
 const LayoutEditorSidebarRight: FC = () => {
+  const { element } = useElement();
   return (
     <AtomWrapper
       backgroundColor="#202020"
@@ -10,7 +22,7 @@ const LayoutEditorSidebarRight: FC = () => {
       alignItems="flex-start"
       padding="1.3rem"
     >
-      <SidebarTabsRight />
+      {element?.id ? layoutTabs?.[element?.tool] : <StageSidebarRight />}
     </AtomWrapper>
   );
 };
