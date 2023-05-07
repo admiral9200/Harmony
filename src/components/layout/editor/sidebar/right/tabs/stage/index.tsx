e058511771d3bf659d190c445fbd79ecdce3de03
Whil <miltonivan117@gmail.com>
2023-05-07 00:55:43 -0600

    add sidebar component properties right

---
+++ src/components/layout/editor/sidebar/right/tabs/stage/index.tsx
@@ -0,0 +1,65 @@
+import useStageConfig from "@/hooks/useStage";
+import { css } from "@emotion/react";
+import { AtomInput, AtomText, AtomWrapper } from "lucy-nxtjs";
+import { FC } from "react";
+
+const StageSidebarRight: FC = () => {
+  const { setConfig, config } = useStageConfig();
+
+  return (
+    <AtomWrapper height="100%" justifyContent="flex-start">
+      <AtomText color="white" fontWeight="bold" fontSize="17px">
+        Stage Properties
+      </AtomText>
+      <AtomInput
+        type="color"
+        label="Background"
+        customCSSLabel={(css) => css`
+          color: white;
+        `}
+        customCSS={(css) => css`
+          height: 100px;
+        `}
+        customWrapperCSS={(css) => css`
+          height: auto;
+        `}
+        css={() => css`
+          height: auto;
+        `}
+        value={config.backgroundColor}
+        onChange={(event) =>
+          setConfig((prev) => ({
+            ...prev,
+            backgroundColor: event.target.value,
+          }))
+        }
+      />
+      <AtomInput
+        type="checkbox"
+        label="Graphic map design"
+        customCSSLabel={(css) => css`
+          color: white;
+        `}
+        customCSS={(css) => css`
+          /* height: 100px; */
+          width: auto;
+        `}
+        customWrapperCSS={(css) => css`
+          height: auto;
+        `}
+        css={() => css`
+          height: auto;
+        `}
+        value={config.Graphicmapdesign}
+        onChange={(event) =>
+          setConfig((prev) => ({
+            ...prev,
+            Graphicmapdesign: !prev.Graphicmapdesign,
+          }))
+        }
+      />
+    </AtomWrapper>
+  );
+};
+
+export default StageSidebarRight;
