fcffd21b60a68fee81acb9ee5aafbea5c7dd28fd
Whil <miltonivan117@gmail.com>
2023-05-07 00:53:28 -0600

    sidebar stageprops

--- src/components/layout/editor/sidebar/left/index.tsx
+++ src/components/layout/editor/sidebar/left/index.tsx
@@ -1,4 +1,4 @@
-import { AtomWrapper } from "lucy-nxtjs";
+import { AtomText, AtomWrapper } from "lucy-nxtjs";
 import { FC } from "react";
 import ElementsList from "./elements";
 
@@ -10,6 +10,14 @@ const LayoutEditorSidebarLeft: FC = () => {
       alignItems="flex-start"
       padding="1.3rem"
     >
+      <AtomText
+        color="white"
+        fontWeight="bold"
+        fontSize="17px"
+        margin="0px 0px 15px 0px"
+      >
+        Stage Properties
+      </AtomText>
       <ElementsList />
     </AtomWrapper>
   );
