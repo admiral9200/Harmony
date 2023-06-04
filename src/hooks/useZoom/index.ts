8a2ba26dc37dba18f747eb8dc381b247d4566f69
whil <miltonivan117@gmail.com>
2023-06-04 10:13:17 -0600

    useZOOM FIX ELEMENTS WITH EVENTS KEYBOARD

--- src/hooks/useZoom/index.ts
+++ src/hooks/useZoom/index.ts
@@ -1,8 +1,9 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useAtom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import Konva from "konva";
 import { KonvaEventObject } from "konva/lib/Node";
-import { Vector2d } from "konva/lib/types";
+import { useEffect, useState } from "react";
 
 const zoomAtom = atomWithStorage("harmony_zoom", {
   scale: 1,
@@ -12,40 +13,67 @@ const zoomAtom = atomWithStorage("harmony_zoom", {
 
 const useZoom = () => {
   const [stage, setStage] = useAtom(zoomAtom);
+  const [scrollEvent, setScrollEvent] = useState<
+    "SCROOLL_VERTICAL" | "SCROOLL_HORIZONTAL" | "SCROOLL_ZOOM"
+  >("SCROOLL_VERTICAL");
 
-  const scaleRelativeToPoint = (
-    point: Vector2d,
-    increaseScale: boolean,
-    event: KonvaEventObject<WheelEvent>
-  ) => {
-    const scaleBy = 1.13;
-    const stage = event.target?.getStage() as Konva.Stage;
-    const oldScale = stage?.scaleX?.();
-    const mousePointTo = {
-      x: point.x / oldScale - stage.x() / oldScale,
-      y: point.y / oldScale - stage.y() / oldScale,
-    };
-    const newScale = increaseScale ? oldScale * scaleBy : oldScale / scaleBy;
+  const handlwRealWheel = (event: KonvaEventObject<WheelEvent>) => {
+    if (scrollEvent === "SCROOLL_VERTICAL") {
+      setStage((prevScrollPosition) => ({
+        ...prevScrollPosition,
+        y: -event.evt.deltaY + prevScrollPosition.y,
+      }));
+    }
 
-    const isValidScale = newScale < 6 && newScale > 0.04168314407042724;
+    if (scrollEvent === "SCROOLL_HORIZONTAL") {
+      setStage((prevScrollPosition) => ({
+        ...prevScrollPosition,
+        x: -event.evt.deltaY + prevScrollPosition.x,
+      }));
+    }
+    if (scrollEvent === "SCROOLL_ZOOM") {
+      const scaleBy = 1.1;
+      const stage = event.target?.getStage() as Konva.Stage;
+      const oldScale = stage.scaleX();
 
-    if (isValidScale) {
-      setStage({
-        scale: newScale,
-        x: (point.x / newScale - mousePointTo.x) * newScale,
-        y: (point.y / newScale - mousePointTo.y) * newScale,
-      });
+      const newScale =
+        event.evt.deltaY < 0 ? oldScale * scaleBy : oldScale / scaleBy;
+      const isValidScale = newScale < 6 && newScale > 0.04168314407042724;
+      setStage((prev) => ({
+        ...prev,
+        scale: isValidScale ? newScale : prev.scale,
+      }));
     }
   };
 
-  const handlwRealWheel = (event: KonvaEventObject<WheelEvent>) => {
-    event.evt.preventDefault();
-    scaleRelativeToPoint(
-      event?.target?.getStage()?.getPointerPosition() as Vector2d,
-      event.evt.deltaY < 0,
-      event
-    );
-  };
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const KEY = event.key?.toUpperCase();
+
+      console.log(KEY);
+
+      if (KEY === "SHIFT") {
+        setScrollEvent("SCROOLL_HORIZONTAL");
+      }
+      if (KEY === "ALT") {
+        setScrollEvent("SCROOLL_VERTICAL");
+      }
+      if (KEY === "CONTROL") {
+        setScrollEvent("SCROOLL_ZOOM");
+      }
+    };
+
+    const handleKeyUp = () => {
+      setScrollEvent("SCROOLL_VERTICAL");
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return {
     onWheel: handlwRealWheel,
     stage,
