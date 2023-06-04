a7a9501aa3750597922de9adc54a6eb1f8524262
whil <miltonivan117@gmail.com>
2023-06-04 10:13:46 -0600

    DISABLE ZOOM  APP

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,13 +3,42 @@ import LayoutFC from "@/layout";
 import "@/styles/globals.css";
 import { ApolloProvider } from "@apollo/client";
 import type { AppPropsWithLayout } from "next/app";
+import Head from "next/head";
+import { useEffect } from "react";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
+  useEffect(() => {
+    const handleWheel = (event: WheelEvent) => {
+      if (event.ctrlKey) {
+        event.preventDefault();
+      }
+    };
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (
+        event.ctrlKey &&
+        (event.key === "-" || event.key === "+" || event.key === "=")
+      ) {
+        event.preventDefault();
+      }
+    };
+
+    document.addEventListener("wheel", handleWheel, { passive: false });
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("wheel", handleWheel);
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
   return (
     <ApolloProvider client={client}>
+      <Head>
+        <meta name="viewport" content="width=device-width, user-scalable=no" />
+      </Head>
       <ToastContainer />
       <LayoutFC {...Component}>
         <Component {...pageProps} />
