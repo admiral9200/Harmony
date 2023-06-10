78f18f903202efbac99f68a10f4901ddf9291e4f
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-06-09 23:08:14 -0600

    sidebar export to pdf

--- src/components/layout/editor/sidebar/right/tabs/stage/index.tsx
+++ src/components/layout/editor/sidebar/right/tabs/stage/index.tsx
@@ -1,11 +1,20 @@
 import useStageConfig from "@/hooks/useStage";
+import useElements from "@/hooks/useStatement";
 import { css } from "@emotion/react";
-import { AtomInput, AtomText, AtomWrapper } from "lucy-nxtjs";
+import jsPDF from "jspdf";
+import {
+  AtomButton,
+  AtomInput,
+  AtomText,
+  AtomWrapper,
+  convertHexToRGB,
+} from "lucy-nxtjs";
 import { FC } from "react";
 
 const StageSidebarRight: FC = () => {
   const { setConfig, config } = useStageConfig();
 
+  const { elements } = useElements();
   return (
     <AtomWrapper height="100%" justifyContent="flex-start">
       <AtomText color="white" fontWeight="bold" fontSize="17px">
@@ -58,8 +67,116 @@ const StageSidebarRight: FC = () => {
           }))
         }
       />
+      <AtomInput
+        type="select"
+        label="FORMAT"
+        customWrapperCSS={() => css`
+          height: auto;
+          label {
+            color: white;
+          }
+        `}
+        customCSS={(css) => css`
+          color: white;
+          height: auto;
+          option {
+            color: black;
+          }
+        `}
+        css={() => css`
+          color: white;
+        `}
+        options={() => [
+          {
+            id: "1",
+            label: "WEB DESIGN",
+            value: "WEB_DESIGN",
+          },
+          {
+            id: "1",
+            label: "PDF DESIGN",
+            value: "PDF_DESIGN",
+          },
+        ]}
+      />
+
+      <AtomButton
+        backgroundColor="#0496ff"
+        onClick={() => {
+          var doc = new jsPDF({
+            orientation: "portrait",
+            unit: "pt",
+            format: [1122, 793],
+          });
+
+          for (const iterator of elements) {
+            const x = convertPxToPt(iterator.x);
+            const y = convertPxToPt(iterator.y);
+            const rgb =
+              convertHexToRGB?.(
+                iterator.style?.colorText ??
+                  iterator.style?.backgroundColor ??
+                  iterator?.style?.stroke ??
+                  ("#000000" as string)
+              ) ?? "#000000";
+
+            const r = rgb?.r || 0;
+            const g = rgb?.g || 0;
+            const b = rgb?.b || 0;
+
+            const width = convertPxToPt(Number(iterator.width));
+            const height = convertPxToPt(Number(iterator.height));
+            if (iterator.tool === "LINE") {
+              doc.setDrawColor(0);
+
+              doc.setFillColor?.(r, g, b);
+              doc.rect(
+                x,
+                y,
+                width,
+                convertPxToPt(Number(iterator.style?.strokeWidth)),
+                "F"
+              );
+            }
+
+            if (iterator.tool === "BOX") {
+              doc.setDrawColor(0);
+              doc.setFillColor?.(r, g, b);
+              doc.rect(x, y, width, height, "F");
+            }
+            if (iterator.tool === "TEXT") {
+              doc.setFontSize(
+                convertPxToPt(iterator.style?.fontSize as number)
+              );
+              doc.setTextColor?.(r, g, b);
+              doc.setFont("helvetica", iterator.style?.fontStyle);
+              doc.text(iterator.text as string, x, y, {
+                maxWidth: width,
+              });
+            }
+
+            if (iterator.tool === "CIRCLE") {
+              doc.setFillColor?.(r, g, b);
+
+              doc.roundedRect(x, y, width, height, width / 2, height / 2, "F");
+            }
+            if (iterator.tool === "IMAGE") {
+              doc.addImage(iterator.src as string, "JPEG", x, y, width, height);
+            }
+          }
+
+          doc.save("harmony");
+        }}
+      >
+        EXPORT PDF
+      </AtomButton>
     </AtomWrapper>
   );
 };
+
+function convertPxToPt(px: number) {
+  const pt = px * 0.75;
+  return pt;
+}
 
 export default StageSidebarRight;
