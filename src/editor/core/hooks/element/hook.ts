a9beee8289ea4ab313fb12e9e94754b7d7b12627
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-07-05 12:09:38 -0600

    change spread operator with ObjectAssign useElement

--- src/editor/core/hooks/element/hook.ts
+++ src/editor/core/hooks/element/hook.ts
@@ -1,34 +1,46 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useAtom } from "jotai";
+import { useAtom, useSetAtom } from "jotai";
 import { useMemo } from "react";
-import { IElement } from "../../elements/type";
+import { IElement, IParamsElement } from "../../elements/type";
+import selectChangeAtom from "../select/jotai";
 import elementSelectedAtom from "./jotai";
 
 const useElement = () => {
   const [element, setElement] = useAtom(elementSelectedAtom);
 
+  const setterChangeElement = useSetAtom(selectChangeAtom);
+
   const handleChangeElement = useMemo(() => {
-    return (params?: IElement) => {
+    return (params: IElement | IParamsElement) => {
       setElement((prev) => {
-        return {
-          ...prev,
-          ...params,
-        };
+        return Object.assign({}, prev, params);
       });
     };
   }, []);
-  const handleSetElement = useMemo(() => {
-    return (element: IElement) => {
-      setElement(element);
+
+  const handleSelectedChangeElement = useMemo(() => {
+    return (params: IElement | IParamsElement) => {
+      setterChangeElement((prev) => {
+        return Object.assign({}, prev, params);
+      });
     };
   }, []);
+
+  const handleSetElement = useMemo(() => {
+    return (element: IElement | IParamsElement) => {
+      setElement(element);
+      setterChangeElement(element);
+    };
+  }, []);
+
   const handleEmptyElement = useMemo(() => {
     return () => {
-      setElement({} as IElement);
+      setElement({} as IElement | IParamsElement);
     };
   }, []);
   return {
     element,
+    handleSelectedChangeElement,
     handleChangeElement,
     handleEmptyElement,
     handleSetElement,
