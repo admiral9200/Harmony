5c12262b264323f6966d4db8f4712a2b5b2dafa3
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-07-09 15:41:40 -0600

    add threads text

--- src/components/layout/editor/sidebar/right/tabs/elements/global/export/threads/threads.ts
+++ src/components/layout/editor/sidebar/right/tabs/elements/global/export/threads/threads.ts
@@ -3,12 +3,14 @@ import { IKeyTool } from "@/editor/core/hooks/tool/types";
 import { Image } from "konva/lib/shapes/Image";
 import { Line } from "konva/lib/shapes/Line";
 import { Rect } from "konva/lib/shapes/Rect";
+import { Text } from "konva/lib/shapes/Text";
+import ThreadText from "./thread-txt";
 import ThreadBox from "./threads-box";
 import ThreadCircle from "./threads-circle";
 import ThreadDraw from "./threads-draw";
 import ThreadImg from "./threads.image";
 
-export type Threads = Rect | Image | Line;
+export type Threads = Rect | Image | Line | Text;
 
 type IThreads = {
   [key in IKeyTool]?: (element: IElement | Partial<IElement>) => Threads;
@@ -19,6 +21,7 @@ const threads: IThreads = {
   IMAGE: ThreadImg,
   CIRCLE: ThreadCircle,
   DRAW: ThreadDraw,
+  TEXT: ThreadText,
 };
 
 export default threads;
