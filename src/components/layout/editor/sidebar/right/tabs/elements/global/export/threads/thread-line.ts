fc917bafefeba70a526746cad25a2e2d973d122d
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-07-09 17:05:52 -0600

    thread line

---
+++ src/components/layout/editor/sidebar/right/tabs/elements/global/export/threads/thread-line.ts
@@ -0,0 +1,15 @@
+import { IElement } from "@/editor/core/elements/type";
+import Konva from "konva";
+
+const ThreadLine = (element: IElement | Partial<IElement>) => {
+  const line = new Konva.Line({
+    id: element?.id,
+    fill: element?.style?.stroke,
+    height: element?.height,
+    width: element?.width,
+    shadowBlur: element?.style?.shadowBlur,
+  });
+  return line;
+};
+
+export default ThreadLine;
