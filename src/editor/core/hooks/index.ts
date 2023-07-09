d3f2f6d463fb7b0d0589ab474ddfc48c4b88e7ae
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-07-09 15:45:02 -0600

    index useSelection hook

--- src/editor/core/hooks/index.ts
+++ src/editor/core/hooks/index.ts
@@ -1,5 +1,6 @@
 export { default as useElement } from "./element/hook";
 export { default as useEvent } from "./events/hook";
+export { default as useSelection } from "./selection/hook";
 export { default as useStage } from "./stage/hook";
 export { default as useTool } from "./tool/hook";
 export { default as useZoom } from "./zoom/hook";
