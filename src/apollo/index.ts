cfc69f801f4e29767356252b734fdb84b9a259da
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-09-03 11:27:12 -0600

    remvove graphql

--- src/apollo/index.ts
+++
@@ -1,8 +0,0 @@
-import CONFIG from "@/config";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: CONFIG.GRAPHQL_URL,
-  cache: new InMemoryCache(),
-});
-export default client;
