d957c3782a702030c00042ee8a7f3e65ed18dad8
Milton Ivan (WhiL) García López <67771688+Whil117@users.noreply.github.com>
2023-09-02 22:17:10 -0600

    update dependency

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,5 @@
-import client from "@/apollo";
 import LayoutFC from "@/layout";
 import "@/styles/globals.css";
-import { ApolloProvider } from "@apollo/client";
 import type { AppPropsWithLayout } from "next/app";
 import Head from "next/head";
 import { useEffect } from "react";
@@ -38,75 +36,66 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
   return (
     <PlausibleProvider domain="harmony.whil.online">
       <Provider>
-        <ApolloProvider client={client}>
-          <Head>
-            <title>Harmony Web Editor Design</title>
-            {/* {props.icon && (
+        <Head>
+          <title>Harmony Web Editor Design</title>
+          {/* {props.icon && (
       <link rel="icon" type="image/png" href={props.icon ?? `/favicon.png`} />
     )} */}
-            <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
-            <meta httpEquiv="X-UA-Compatible" content="IE=7" />
-            <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-            <meta name="canonical" content="https://harmony.whil.online/" />
-            <meta
-              name="keywords"
-              content={`react reactkonva next.js typescript jotai`}
-            />
-            <meta
-              name="viewport"
-              content="width=device-width, initial-scale=1"
-            />
-            <link rel="manifest" href="/manifest.json" />
-            <meta
-              name="description"
-              content="Harmony offers an innovative solution for React projects that require image editing, PDF creation, and dynamic web design capabilities, similar to popular tools like Figma, Adobe XD, and Adobe Photoshop"
-            />
-            <meta name="title" content="Harmony Web Editor Design" />
-            <meta
-              name="description"
-              content="Harmony offers an innovative solution for React projects that require image editing, PDF creation, and dynamic web design capabilities, similar to popular tools like Figma, Adobe XD, and Adobe Photoshop"
-            />
-            <meta name="googlebot" content="index,follow" />
+          <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=7" />
+          <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+          <meta name="canonical" content="https://harmony.whil.online/" />
+          <meta
+            name="keywords"
+            content={`react reactkonva next.js typescript jotai`}
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="Harmony offers an innovative solution for React projects that require image editing, PDF creation, and dynamic web design capabilities, similar to popular tools like Figma, Adobe XD, and Adobe Photoshop"
+          />
+          <meta name="title" content="Harmony Web Editor Design" />
+          <meta
+            name="description"
+            content="Harmony offers an innovative solution for React projects that require image editing, PDF creation, and dynamic web design capabilities, similar to popular tools like Figma, Adobe XD, and Adobe Photoshop"
+          />
+          <meta name="googlebot" content="index,follow" />
 
-            <meta property="og:locale" content="ES" />
-            <meta property="og:type" content="article" />
-            <meta property="og:title" content="Harmony Web Editor Design" />
-            <meta
-              property="og:description"
-              content="Harmony offers an innovative solution for React projects that require image editing, PDF creation, and dynamic web design capabilities, similar to popular tools like Figma, Adobe XD, and Adobe Photoshop"
-            />
-            <meta property="og:url" content="https://harmony.whil.online/" />
-            <meta property="og:site_name" content="Harmony Web Editor Design" />
-            <meta property="og:image" content="/coverseo2.png" />
+          <meta property="og:locale" content="ES" />
+          <meta property="og:type" content="article" />
+          <meta property="og:title" content="Harmony Web Editor Design" />
+          <meta
+            property="og:description"
+            content="Harmony offers an innovative solution for React projects that require image editing, PDF creation, and dynamic web design capabilities, similar to popular tools like Figma, Adobe XD, and Adobe Photoshop"
+          />
+          <meta property="og:url" content="https://harmony.whil.online/" />
+          <meta property="og:site_name" content="Harmony Web Editor Design" />
+          <meta property="og:image" content="/coverseo2.png" />
 
-            <meta property="og:image:secure_url" content="/coverseo2.png" />
-            <meta property="og:image:width" content="1200" />
-            <meta property="og:image:height" content="630" />
-            <meta property="og:image:alt" content="Harmony Web Editor Design" />
+          <meta property="og:image:secure_url" content="/coverseo2.png" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="630" />
+          <meta property="og:image:alt" content="Harmony Web Editor Design" />
 
-            <meta name="twitter:card" content="summary_large_image" />
-            <meta
-              name="twitter:description"
-              content="Harmony offers an innovative solution for React projects that require image editing, PDF creation, and dynamic web design capabilities, similar to popular tools like Figma, Adobe XD, and Adobe Photoshop"
-            />
-            <meta name="twitter:title" content="Harmony Web Editor Design" />
-            <meta name="twitter:image" content="/coverseo2.png" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="twitter:description"
+            content="Harmony offers an innovative solution for React projects that require image editing, PDF creation, and dynamic web design capabilities, similar to popular tools like Figma, Adobe XD, and Adobe Photoshop"
+          />
+          <meta name="twitter:title" content="Harmony Web Editor Design" />
+          <meta name="twitter:image" content="/coverseo2.png" />
 
-            <meta
-              property="og:image"
-              itemProp="image"
-              content="/coverseo2.png"
-            />
-            <meta property="og:image:secure_url" content="/coverseo2.png" />
-            <meta property="og:image:type" content="image/png" />
-            <meta property="og:image:width" content="300" />
-            <meta property="og:image:height" content="300" />
-          </Head>
-          <ToastContainer />
-          <LayoutFC {...Component}>
-            <Component {...pageProps} />
-          </LayoutFC>
-        </ApolloProvider>
+          <meta property="og:image" itemProp="image" content="/coverseo2.png" />
+          <meta property="og:image:secure_url" content="/coverseo2.png" />
+          <meta property="og:image:type" content="image/png" />
+          <meta property="og:image:width" content="300" />
+          <meta property="og:image:height" content="300" />
+        </Head>
+        <ToastContainer />
+        <LayoutFC {...Component}>
+          <Component {...pageProps} />
+        </LayoutFC>
       </Provider>
     </PlausibleProvider>
   );
